5bf2df595545b183cf9940a1b547f237d1eeed99
Kengatharan Mahinthan <Mahinthan@Kengatharans-MacBook-Pro.local>
2018-11-10 13:40:10 +0100

    Added the first part of the test Form

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,5 @@
+
+
 function initMap() {
     // The location of Paris
     var location = {lat: 48.852767, lng: 2.348584};
@@ -6,6 +8,7 @@ function initMap() {
         document.getElementById('map'), {zoom: 12, center: location});
     // The marker, positioned at Paris
     var marker = new google.maps.Marker({position: location, map: map});
+
 
     google.maps.event.addListener(map, 'click', function(e) {
         latLng = e.latLng;
@@ -18,19 +21,46 @@ function initMap() {
             position: latLng,
             map: map,
         });
-
-        fire_api(e.latLng.lat(), e.latLng.lng())
+        var data = {
+            'lat' : e.latLng.lat(),
+            'lng' : e.latLng.lng(),
+        };
+        fire_ajax_request(data)
     });
 
 }
+// Handle form
+$("#mainForm").submit(function( event ) {
 
-function fire_api(lat, lng){
-    var endpoint = "/data";
-    $.getJSON( endpoint, { lat: lat, lng: lng } )
-        .done(function( json ) {
-            $('.result').html("<h3>" + json.result + "</h3>")
-        })
-        .fail(function( jqxhr, textStatus, error ) {
-            $('.result').html("<h3> Oopsy! out of coverage area :(</h3>")
-        });
+    var formData = {
+        'lat' : $('#latValue').val(),
+        'lng' : $('#lngValue').val(),
+    };
+
+    fire_ajax_request(formData);
+    // stop the form from submitting the normal way and refreshing the page
+    event.preventDefault();
+});
+
+
+function fire_ajax_request(coor_object){
+    // process the form
+    $.ajax({
+        type        : 'GET', // define the type of HTTP verb we want to use (POST for our form)
+        url         : '/data', // the url where we want to POST
+        data        : coor_object, // our data object
+        dataType    : 'json', // what type of data do we expect back from the server
+        encode          : true,
+        statusCode: {
+            200: function (response) {
+                $('.result').html("<h3>" + response.result + "</h3>")
+            },
+            403: function (response) {
+                $('.result').html("<h3> Oopsy! out of coverage area :(</h3>")
+            },
+            500: function (response) {
+                $('.result').html("<h3> Missing or incorrect values</h3>")
+            }
+        }
+    });
 }
